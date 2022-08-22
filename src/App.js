fd6db7bf6fd18fff06e103062ab2d821dc027d08
val3r0 <danielgvalero@pm.me>
2022-08-22 22:04:54 +1000

    test1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
+const ax = axios({
+  method: "get",
+  url: "https://connect.squareup.com/v2/payments",
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Content-Type': 'application/json',
+    Authorization: `Bearer EAAAEEZJfISawonwneMQCyJ2ZDsj60AVk0FKyNyd-d6wLt31Z3Fd45HvfajbJwo1`,
+    withCredentials: true,
+    mode: 'no-cors',
+  },
+}).then((response) => {
+  console.log(response.data);
+});
+
+console.log(ax)
 
 function App() {
   return (
